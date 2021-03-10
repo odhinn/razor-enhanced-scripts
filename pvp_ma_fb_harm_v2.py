6d915155078f2dc305f18dc142e91af1027425b1
odhinn <mightyodhinn@gmail.com>
2021-03-10 21:26:42 +0300

    Create pvp_ma_fb_harm_v2.py
    if you have an issue with harm casting you can try this version.

---
+++ pvp_ma_fb_harm_v2.py
@@ -0,0 +1,48 @@
+##############################################################################################
+#                   PVP - Chaining Magic Arrow and Fireball with Harm V2                     #
+##############################################################################################
+# Authors   : Odhinn, Mourn                                                                  #
+# Server    : Quingis | htttp://www.quingis.com                                              # 
+# Info      : This script let you chaining Magic Arrow and Fireball and cast Harm spell      #
+#             if you close to enemy. There is no rapid repetition of magic arrow so spamming #
+#             it useless except initial hit.                                                 #
+#             So we are combining it with Fireball, Magic Arrow chain. You can also use this #
+#             for fizzle your oppenent while doing some moderate damage.                     #
+#                                                                                            #
+# Usage     : Assign Hotkey and use it. You can spam while pressing the key.                 #
+# Warning   : You have to enable "Wait before interrupt" in Scripting/Script Info            #
+# Source    : This script converted from UOSteam to Razor Enhanced                           #
+##############################################################################################
+
+
+SharedDataName = "Magic Arrow"
+
+if not Misc.CheckSharedValue(SharedDataName):
+    Misc.SetSharedValue(SharedDataName,"Magic Arrow")
+
+def controlSpell():
+    return Misc.ReadSharedValue(SharedDataName)
+
+    
+def checkPoint(spell):
+    Misc.SetSharedValue(SharedDataName,spell)
+
+enemy = Target.GetLast()
+
+if Player.InRangeMobile(enemy,10):
+    if Player.InRangeMobile(enemy,1):
+        Spells.CastMagery("Harm")
+        Target.WaitForTarget(5000,True)
+        Target.TargetExecute(enemy)
+    else:
+        spellcast = controlSpell()
+        
+        Spells.CastMagery(spellcast)
+        Target.WaitForTarget(5000,True)
+        Target.TargetExecute(enemy)
+
+        
+        if controlSpell() == "Magic Arrow":
+           checkPoint("Fireball")
+        else:
+           checkPoint("Magic Arrow")
